6278b980d685822aaba169e49afe746dfeb3235c
Aruni Mishra <arunimishramsit@gmail.com>
2022-01-11 18:24:26 +0100

    ServiceBusMessage

--- AzureFunctionServiceBus/ServiceBusQueueTrigger/__init__.py
+++ AzureFunctionServiceBus/ServiceBusQueueTrigger/__init__.py
@@ -4,5 +4,6 @@ import azure.functions as func
 
 
 def main(msg: func.ServiceBusMessage):
-    logging.info('Python ServiceBus queue trigger processed message: %s',
+    logging.info('Python ServiceBus queue trigger processed message: %s %s',
+                msg.content_type,
                  msg.get_body().decode('utf-8'))
