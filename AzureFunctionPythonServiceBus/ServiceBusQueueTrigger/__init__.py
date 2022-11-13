b7d741cff050b84748e589d23485e2c3574fbd77
Aruni Mishra <arunimishramsit@gmail.com>
2022-11-13 22:00:23 +0530

    opencensus-extension-azure-functions

--- AzureFunctionPythonServiceBus/ServiceBusQueueTrigger/__init__.py
+++ AzureFunctionPythonServiceBus/ServiceBusQueueTrigger/__init__.py
@@ -1,23 +1,18 @@
 import logging
 
 import azure.functions as func
-from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.extension.azure.functions import OpenCensusExtension
 
 logger = logging.getLogger(__name__)
 
-# TODO: replace the all-zero GUID with your instrumentation key.
-logger.addHandler(AzureLogHandler(
-    connection_string='InstrumentationKey=f413564b-5bc9-43ed-912b-ecb58407f347')
-)
+OpenCensusExtension.configure()
 
 
-def main(msg: func.ServiceBusMessage):
+def main(msg: func.ServiceBusMessage, context):
     logger.warning('logger: Python ServiceBus queue trigger processed message: %s %s',
                    msg.content_type,
                    msg.get_body().decode('utf-8'))
 
-    properties = {'custom_dimensions': {
-        'key_1': 'value_1', 'key_2': 'value_2'}}
-
-    # Use properties in logging statements
-    logger.warning('action', extra=properties)
+    with context.tracer.span("parent"):
+        logger.info('Message from ServiceBus queue trigger %s %s %s %s !', context.function_name,
+                    context.function_directory, context.invocation_id, context.trace_context.Tracestate)
--- AzureFunctionPythonServiceBus/ServiceBusQueueTrigger/log.py
+++ AzureFunctionPythonServiceBus/ServiceBusQueueTrigger/log.py
@@ -1,19 +1,21 @@
-import logging
-from opencensus.ext.azure.log_exporter import AzureLogHandler
-
-logger = logging.getLogger(__name__)
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
 
 # TODO: replace the all-zero GUID with your instrumentation key.
-logger.addHandler(AzureLogHandler(
-    connection_string='InstrumentationKey=f413564b-5bc9-43ed-912b-ecb58407f347')
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=f413564b-5bc9-43ed-912b-ecb58407f347'),
+    sampler=ProbabilitySampler(1.0),
 )
 # You can also instantiate the exporter directly if you have the environment variable
 # `APPLICATIONINSIGHTS_CONNECTION_STRING` configured
-# logger.addHandler(AzureLogHandler())
+# exporter = AzureExporter()
 
 def valuePrompt():
-    line = input("Enter a value: ")
-    logger.warning(line)
+    with tracer.span(name="test") as span:
+        line = input("Enter a value now: ")
+        print(line)
 
 def main():
     while True:
